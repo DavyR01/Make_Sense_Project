4aa587d65960f227c289e2a1c4fc2aaabc855962
Davy ROBERT <davy.robert@bluesoft-group.com>
2023-08-10 14:53:47 +0200

    modifications a implementer sur dev

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,8 @@
-require("dotenv").config();
+const dotenv = require("dotenv").config();
 
 const app = require("./src/app");
 
-const port = parseInt(process.env.APP_PORT ?? "5000", 10);
+const port = parseInt(dotenv.APP_PORT ?? "5000", 10);
 
 app.listen(port, (err) => {
   if (err) {
