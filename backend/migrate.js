2d43ed2ad19f835ea1b4215a1af5e796a441a34c
Davy <100140271+DavyR01@users.noreply.github.com>
2024-12-05 14:19:37 +0100

    add modifications migrate.js

--- backend/migrate.js
+++ backend/migrate.js
@@ -6,22 +6,29 @@ const mysql = require("mysql2/promise");
 const migrate = async () => {
   const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
 
-  const connection = await mysql.createConnection({
-    host: DB_HOST,
-    user: DB_USER,
-    password: DB_PASSWORD,
-    multipleStatements: true,
-  });
+  let connection;
+  try {
+    connection = await mysql.createConnection({
+      host: DB_HOST,
+      user: DB_USER,
+      password: DB_PASSWORD,
+      multipleStatements: true,
+    });
 
-  await connection.query(`drop database if exists ${DB_NAME}`);
-  await connection.query(`create database ${DB_NAME}`);
-  await connection.query(`use ${DB_NAME}`);
+    await connection.query(`drop database if exists ${DB_NAME}`);
+    await connection.query(`create database ${DB_NAME}`);
+    await connection.query(`use ${DB_NAME}`);
 
-  const sql = fs.readFileSync("./database.sql", "utf8");
+    const sql = fs.readFileSync("./database.sql", "utf8");
 
-  await connection.query(sql);
-
-  connection.end();
+    await connection.query(sql);
+  } catch (err) {
+    console.error("Migration error:", err);
+  } finally {
+    if (connection) {
+      await connection.end();
+    }
+  }
 };
 
 try {
