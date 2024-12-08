f7d882649e2824fef2cc4ec55516e121bd8d5218
Davy <100140271+DavyR01@users.noreply.github.com>
2024-12-08 20:31:50 +0100

    handle click outside window notification with useRef

--- frontend/src/components/user/NotificationModal.jsx
+++ frontend/src/components/user/NotificationModal.jsx
@@ -1,5 +1,5 @@
 import { Button, Modal } from "flowbite-react";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useCurrentDarkContext } from "../../context/DarkContext";
 import { useCurrentUserContext } from "../../context/UserContext";
@@ -7,11 +7,12 @@ import "../../css/user/sidebar.css";
 
 const backEnd = import.meta.env.VITE_BACKEND_URL;
 
-function NotificationModal({ setShowModal, open, showModal }) {
+function NotificationModal({ setShowModal, showModal }) {
   const { user, token } = useCurrentUserContext();
   const { t } = useTranslation();
   const [notifs, setNotifs] = useState();
   const { dark } = useCurrentDarkContext();
+  const modalRef = useRef(null);
 
   useEffect(() => {
     const myHeader = new Headers();
@@ -27,6 +28,21 @@ function NotificationModal({ setShowModal, open, showModal }) {
       .catch((error) => console.warn("error", error));
   }, [token]);
 
+  const handleClickOutside = (event) => {
+    if (modalRef.current && !modalRef.current.contains(event.target)) {
+      setShowModal(false);
+    }
+  };
+
+  useEffect(() => {
+    if (showModal) {
+      document.addEventListener("mousedown", handleClickOutside);
+    }
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [showModal]);
+
   return (
     <div className="fixed top-0 left-0 z-10">
       {showModal && (
@@ -34,21 +50,13 @@ function NotificationModal({ setShowModal, open, showModal }) {
         //   <div className="bg-dark-header fixed inset-0 bg-opacity-50 transition-opacity" />
       )}
       <Modal
-        //   style={{
-        //     width: "100%",
-        //   }}
-
+        //   ref={modalRef}
         //   className={` bg-transparent shake ${
         //     //   className={` bg-green-500 shake ${
         //     open && showModal ? " rounded-r-xl w-full" : ""
         //   }`}
-
-        className={`${
-          open && showModal
-            ? "lg:ml-[274px] lg:mt-[110px] rounded-r-xl shake bg-transparent"
-            : "md:ml-[78px] md:mt-[110px] rounded-r-xl shake"
-        }`}
-        show
+        className="lg:ml-[274px] lg:mt-[110px] rounded-r-xl shake bg-transparent"
+        show={showModal}
         position="top-left"
         //   position={open && showModal ? "center" : "top-left"}
         //   position="top-left"
@@ -56,19 +64,24 @@ function NotificationModal({ setShowModal, open, showModal }) {
         onClose={() => setShowModal(false)}
       >
         <div
+          ref={modalRef}
           //  style={{ width: "400px", marginLeft: "300px", marginTop: "100px" }}
           //  w-[400px] ml-[300px] mt-[100px] md:w-full md:ml-0 md:mt-0
           className={` ${!dark ? "border border-white rounded-lg" : ""}`}
         >
           {/* {document.body.classList.add("!bg-green-500")}; */}
           <Modal.Header
+            // ref={modalRef}
             className={`pl-3 pr-3 pt-6 pb-6  text-slate-50 align-middle rounded-tr-lg ${
               dark ? "bg-light-blue" : "bg-dark-bg"
             }`}
           >
             <div className="text-white">{t("Notifications title")}:</div>
           </Modal.Header>
-          <Modal.Body className={dark ? "bg-gray-200" : "bg-dark-header"}>
+          <Modal.Body
+            // ref={modalRef}
+            className={dark ? "bg-gray-200" : "bg-dark-header"}
+          >
             <div className="space-y-3 p-6 grid grid-cols-1 divide-y text-base leading-relaxed text-gray-500 dark:text-gray-400">
               {notifs?.map((notif) => (
                 <div key={notif.id}>
