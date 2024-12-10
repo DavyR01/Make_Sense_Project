f6960257c98f2e3a9bffdbebece9f432f95d8b23
Davy <100140271+DavyR01@users.noreply.github.com>
2024-12-10 17:19:37 +0100

    - Add features rotate secret key every 15 days still preserving the old token during 15 others days. Keeping a smooth transition.

--- backend/src/middlewares/auth.js
+++ backend/src/middlewares/auth.js
@@ -1,8 +1,68 @@
 require("dotenv").config();
 const argon2 = require("argon2");
 const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
 
-const { JWT_SECRET, JWT_TIMING } = process.env;
+const { /*  JWT_SECRET,  */ JWT_TIMING } = process.env;
+
+//* ************** Handle secret key rotation ********************
+let activeSecrets = [];
+
+function generateNewSecret() {
+  return crypto.randomBytes(32).toString("hex");
+}
+
+function getLatestSecret() {
+  return activeSecrets[0].value;
+}
+
+function initializeSecrets() {
+  const currentSecret = generateNewSecret();
+  const olderSecret = generateNewSecret();
+
+  activeSecrets = [
+    {
+      id: `currentSecret${Date.now()}`,
+      value: currentSecret,
+      expiresAt: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000), // ? 30 days
+    },
+    {
+      id: `oldSecret${Date.now() - 1}`,
+      value: olderSecret,
+      expiresAt: new Date(Date.now() + 15 * 24 * 60 * 60 * 1000), // ? 15 days
+    },
+  ];
+  //   console.log("initialize active Secrets : ", activeSecrets);
+}
+
+function rotateSecrets() {
+  const newSecret = generateNewSecret();
+  //!   console.log("New secret : ", newSecret);
+
+  activeSecrets.unshift({
+    id: `secret${Date.now()}`,
+    value: newSecret,
+    expiresAt: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
+  });
+
+  //   console.log("active Secrets in rotate : ", activeSecrets);
+
+  // ? Remove expirated secrets
+  const now = new Date();
+  activeSecrets = activeSecrets.filter((secret) => secret.expiresAt > now);
+
+  // ? Keep only 2 last objects in array activeSecrets
+  if (activeSecrets.length > 5) {
+    activeSecrets = activeSecrets.slice(0, 5);
+  }
+  //!   console.log("Active Secrets after rotation: ", activeSecrets);
+}
+
+initializeSecrets();
+
+setInterval(rotateSecrets, 15 * 24 * 60 * 60 * 1000); // ? Automatic rotation every 15 days.
+
+//* ************** END Handle secret key rotation ********************
 
 const hashingOptions = {
   type: argon2.argon2id,
@@ -35,7 +95,7 @@ const verifyPassword = async (req, res) => {
       const payload = { sub: req.user.id };
       // console.log("PAYLOAD :", payload);
 
-      const token = jwt.sign(payload, JWT_SECRET, {
+      const token = jwt.sign(payload, getLatestSecret(), {
         algorithm: "HS512",
         expiresIn: JWT_TIMING,
       });
@@ -69,12 +129,26 @@ const verifyToken = async (req, res, next) => {
       throw new Error("Authorization header has not the 'Bearer' type");
     }
 
-    req.payload = await jwt.verify(token, JWT_SECRET);
+    //  req.payload = jwt.verify(token, getLatestSecret());
 
-    next();
+    for (let index = 0; index < activeSecrets.length; index += 1) {
+      try {
+        //   console.log("Active secrets verify token", activeSecrets);
+        //   console.log(`IN LOOP index: ${index} `);
+
+        req.payload = jwt.verify(token, activeSecrets[index].value);
+        return next();
+      } catch (err) {
+        // ? The loop goes on !
+        console.log("The loop goes on !");
+      }
+    }
+
+    return res.status(401).send({ message: "Invalid token !!!!!!" }); // ? Message personnalisé visible dans onglet Network des devtools du navigateur
+    //  throw new Error("Invalid token");
   } catch (err) {
     console.error(err);
-    res.sendStatus(401);
+    return res.sendStatus(500);
   }
 };
 
